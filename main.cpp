7459ad9929ade4004eca7aa7d85613333e5cc25d
Da Teng <dateng2016@gmail.com>
2024-11-23 16:46:43 -0500

    a

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "mpi.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
